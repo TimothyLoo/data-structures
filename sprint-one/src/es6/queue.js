b350dcafe68bdd07c36ffc456fcd7c352053ddc4
Timothy Loo <TimothyLoo07@gmail.com>
2022-07-20 13:37:55 -1000

    update files queue.js & stack.js Sprint 1 ES6 that pass all tests

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,46 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    // Initialize front to track the index of the item at the front of the queue
+    this.front = 0;
+    // Initialize back to track the index of the item at the back of the queue
+    this.back = 0;
+    // Initialize an empty object storage that will hold each item
+    this.storage = {};
+  }
+
+  // Create methods for the instance to use
+
+  // Method enqueue adds an item to the back of the queue
+  enqueue(value) {
+    // Set value equal to the index of the back of the queue
+    this.storage[this.back] = value;
+    // Increment back counter to reflect the next index for item to be added at back of queue
+    this.back++;
+  }
+
+  // Method dequeue removes an item from the front of the queue
+  dequeue() {
+    // If there is something in the queue
+    if (this.back - this.front > 0) {
+      // Set var value equal to the item to be removed from the front of the queue
+      let value = this.storage[this.front];
+      // Remove the item from the front of the queue
+      delete this.storage[this.front];
+      // Increment the front counter to reflect the next item moved to the front of the queue
+      this.front++;
+      // Return the value of the item removed
+      return value;
+    }
+  }
+
+  // Method size returns the size of the queue
+  size() {
+    // Returns the size of the queue
+    return this.back - this.front;
   }
 
 }
+
+// Create instance
+var Instance = new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,43 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    // Initialize a height variable to track the size of the stack
+    this.height = 0;
+    // Initialize an empty storage object
+    this.storage = {};
   }
 
+  // Create methods for the instance to use
+
+  // Method push adds an item to the top of the stack
+  push(value) {
+    // Set value equal to the key at height
+    this.storage[this.height] = value;
+    // Increment the height counter to show the new size of the stack & the next index for new item to be added
+    this.height++;
+  }
+
+  // Method pop removes an item from the top of the stack
+  pop () {
+    // If there is something on the stack
+    if (this.height > 0) {
+      // Decrement the height counter to reflect the new height of the stack and the index of the item to be removed
+      this.height--;
+      // Set var value equal to the item to be removed from the top of the stack
+      let value = this.storage[this.height];
+      // Remove the item at the top of the stack
+      delete this.storage[this.height];
+      // Return the value of the item removed
+      return value;
+    }
+  }
+
+  // Method size returns the size of the stack
+  size() {
+    // return the height of the stack
+    return this.height;
+  }
 }
+
+// Create an instance
+var Instance = new Stack();
