9c1fc24dcddcf17fc1bb39e8312a9823c9caced4
Timothy Loo <TimothyLoo07@gmail.com>
2022-07-20 13:13:11 -1000

    update the files queue.js & stack.js Sprint 1 Pseudoclassical that pass all tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,36 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.front = 0; // Initialize a variable to track the front of the queue
+  this.back = 0; // Initialize a variable to track the back of the queue
+  this.storage = {}; // Initialize an empty object to store the items in the queue
 };
 
 
+// Create methods to be used by the instance
+
+// Method enqueue adds an item to the back of the queue
+Queue.prototype.enqueue = function (value) {
+  this.storage[this.back] = value; // Set value equal the key at the back of the queue
+  this.back++; // Increment the back counter to mark the next index for a new item to be added
+};
+
+// Method dequeue removes an item from the front of the queue
+Queue.prototype.dequeue = function () {
+  // If there is something in the queue
+  if (this.back - this.front) {
+    let value = this.storage[this.front]; // Set var value equal to the item to be removed from the front of the queue
+    delete this.storage[this.front]; // Remove the item from the front of the queue
+    this.front++; // Increment the front counter to indicate the next item moved to the front of the queue
+    return value; // return the value of the item removed
+  }
+};
+
+// Method size returns the size of the queue
+Queue.prototype.size = function () {
+  return this.back - this.front; // Return the size of the queue
+};
+
+// Create an instance
+var Instance = new Queue();
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.height = 0; // Initialize the height of the stack
+  this.storage = {}; // Initialize an empty object of storage items
 };
 
+// Create methods to be used by the instance
 
+// Push method adds an item to the top of the stack
+Stack.prototype.push = function (value) {
+  this.storage[this.height] = value; // Set value to the index height, which adds to the top of the stack
+  this.height++; // Increment the height counter to reflect the size of the stack and the next index for which to add a new item
+};
+
+// Pop method removes an item from the top of the stack
+Stack.prototype.pop = function () {
+  // If there is something on the stack
+  if (this.height > 0) {
+    this.height--; // Decrement the height counter to reflect the new height of the stack, and set to index to the item on the top of the stack
+    let value = this.storage[this.height]; // Set var value to the item being removed from the top of the stack
+    delete this.storage[this.height]; // Remove the item from the top of the stack
+    return value; // Return the value of what was removed from the top of the stack
+  }
+};
+
+// Size method returns the height of the stack
+Stack.prototype.size = function () {
+  return this.height; // Returns the height of the stack
+};
+
+// Create an instance
+var Instance = new Stack();
