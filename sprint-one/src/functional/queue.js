ef153a902600690651a5a47ee6c3e82880224f92
Timothy Loo <TimothyLoo07@gmail.com>
2022-07-20 12:21:41 -1000

    update the files queue.js & stack.js in Sprint 1 Functional that pass all tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,16 +4,34 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var front = 0; // Declare a variable to track the front of the queue
+
+  var back = 0; // Declare variable to track the back of the queue
+
   // Implement the methods below
 
+  // enqueue adds something to the back of the queue
   someInstance.enqueue = function(value) {
+    storage[back] = value; // Add the value to storage at key back
+    back++; // Increase the key index for the next item at the back of the queue
   };
 
+  // dequeue removes something from the front of the queue
   someInstance.dequeue = function() {
+    // if there is something in queue
+    if (back - front > 0) {
+      var value = storage[front]; // Set var value to the item at the front of the queue
+      delete storage[front]; // Remove the item from the front of the queue
+      front++; // Increment the count front to be the next item in the queue
+      return value; // Return what was removed from the front of the queue
+    }
   };
 
+  // reports the size of the queue
   someInstance.size = function() {
+    return back - front;
   };
 
+  // Return instance
   return someInstance;
 };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,26 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var size = 0; // Create a variable size to track the size of the object instance
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value; // add the value to the storage object at index #size
+    size++; // increase the size count to indicate size of storage object and set next key
   };
 
   someInstance.pop = function() {
+    // Check to see if there are any items in the stack. Nothing can be removed if there is nothing in there
+    if (size > 0) {
+      size--; // Decrease the count of the stack
+      var value = storage[size]; // Set a var value to what is going to be removed from the top of the stack
+      delete storage[size]; // Remove the item from the top of the stack
+      return value; // Return what is being removed from the top of the stack
+    }
   };
 
   someInstance.size = function() {
+    return size; // Return the size of the stack
   };
 
   return someInstance;
